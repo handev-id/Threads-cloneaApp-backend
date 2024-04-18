805e7524ea49b960dc0b58d044bc830e598e56b2
misidana <misidana02@gmail.com>
2024-04-18 06:00:42 -0700

    add like and reply controller

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,8 +1,11 @@
 const Post = require("../models/postModel");
+const Reply = require("../models/replyModel");
 
 const getPosts = async (req, res) => {
   try {
-    const posts = await Post.find().populate("userId", "username avatar");
+    const posts = await Post.find().populate("userId", "username avatar").sort({
+      createdAt: -1,
+    });
 
     return res.status(200).json({
       success: true,
@@ -46,7 +49,77 @@ const createPost = async (req, res) => {
   }
 };
 
+const likePost = async (req, res) => {
+  try {
+    const postId = req.params.postId;
+    const userId = req.user.id;
+
+    if (!postId || !userId) {
+      return res.status(400).json({
+        success: false,
+        message: "Post Id harus diisi",
+      });
+    }
+
+    const likes = await Post.findById(postId).select("likes");
+    const likesArray = likes.likes;
+
+    if (likesArray.includes(userId)) {
+      await Post.findByIdAndUpdate(postId, {
+        $pull: { likes: userId },
+      });
+    } else {
+      await Post.findByIdAndUpdate(postId, {
+        $push: { likes: userId },
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      result: likesArray.length,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+const createReply = async (req, res) => {
+  try {
+    const postId = req.params.postId;
+    const userId = req.user.id;
+    const { reply } = req.body;
+
+    if (!postId || !userId || !reply) {
+      return res.status(400).json({
+        success: false,
+        message: "Data Kurang!",
+      });
+    }
+
+    const newReply = await Reply.create({
+      userId,
+      postId,
+      reply,
+    });
+
+    return res.status(200).json({
+      success: true,
+      result: newReply,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
 module.exports = {
   getPosts,
   createPost,
+  likePost,
+  createReply,
 };
--- routes/postRoute.js
+++ routes/postRoute.js
@@ -1,8 +1,15 @@
-const { getPosts, createPost } = require("../controllers/postController");
+const {
+  getPosts,
+  createPost,
+  likePost,
+  createReply,
+} = require("../controllers/postController");
 
 const router = require("express").Router();
 
 router.get("/list", getPosts);
 router.post("/create", createPost);
+router.post("/like/:postId", likePost);
+router.post("/reply/:postId", createReply);
 
 module.exports = router;
